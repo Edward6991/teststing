a25a04858d9d81112b982e6fc2b2ddc30da350b0
Edward6991 <35705026+Edward6991@users.noreply.github.com>
2018-01-23 18:00:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const TOKEN = '536514506:AAGcjUbfTIwguMWMykB2KfldvgpKvcr7MfM'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.onText(/\/start/, msg => {
-    console.log(msg)
+    
+    const text = 'Приветствую, ${msg.from.first_name}\nчто вы хотите делать?'
+    
+    bot.sendMessage(msg.chat.id, text)
   
     })
