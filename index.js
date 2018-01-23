9dec7dd2b7ce40b5a576cded99d1eeb715e3d045
Edward6991 <35705026+Edward6991@users.noreply.github.com>
2018-01-23 17:52:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const TOKEN = '536514506:AAGcjUbfTIwguMWMykB2KfldvgpKvcr7MfM'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello from me, bot says: "Hi, ${msg.from.first_name}"')
+bot.onText(/\/start/, msg => {
+    console.log(msg)
+  
     })
